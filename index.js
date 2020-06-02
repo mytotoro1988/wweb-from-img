4be879f291fcc0832caf3e64da897a6673080918
Cuong <lehuucuong.vnua@gmail.com>
2020-06-02 14:40:15 +0700

    'add-pug'

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(request, response){
-    response.send('Hello Cuong')
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+app.get('/', function(req, res){
+    res.send('Hello Cuong <a href="/users"> Userlist </a>')
+
+})
+app.get('/users', function(req, res){
+    res.send('User list')
 })
 
 app.listen(port, function(){
