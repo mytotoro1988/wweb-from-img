9a503c295984fa12441c5d59254943ae7066b91a
CuongLe <lehuucuong.vnua@gmail.com>
2020-06-04 14:48:57 +0700

    'control-layout-db-view-bootstrap'

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser')
+
+var userRoute = require('./routes/user.route')
+
 var port = 3000;
-var shortid = require('shortid');
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-
-db = low(adapter);
-// Set some defaults (required if your JSON file is empty)
-db.defaults({users:[] })
-  .write();
 
 
+var app = express();
 
 
 
@@ -23,51 +17,11 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 
+app.use('/users', userRoute);
 
 app.get('/', function(req, res){
     res.render('index',{
         name: 'Mei'
-    })
-})
-app.get('/users', function(req, res){
-    res.render('users/index',{
-        users: db.get('users').value()
-    })
-})
-
-
-//tim kiem theo ten
-app.get('/users/search', function(req, res){
-    var q = req.query.q;
-    var matcheUsers = db.get('users').value().filter(function(user){
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    })
-    res.render('users/index',{
-        users :matcheUsers
-    })
-})
-
-
-
-//ttao nguoi dung moi
-app.get('/users/create',function(req,res){
-    res.render('users/create')
-});
-app.post('/users/create',function(req,res){
-    req.body.id = shortid.generate();//tao id ngau nhien bang shortid
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-  // chuyen nguoi dung ve trang truoc
-    
-
-})
-
-
-app.get('/users/:id',function(req,res){//truyen id vao link moi
-    var id = req.params.id;
-    var user = db.get('users').find({id : id}).value();
-    res.render('users/view',{
-        user: user
     })
 })
 
